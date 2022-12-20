dd06ea707a25af311503b19ff7072e2a355b1368
Yevheniia Suska <susskaya86@gmail.com>
2022-12-20 13:50:10 +0200

    deploy 2

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import s from './MovieItem.module.css';
-import { routes } from 'routes';
+// import { routes } from 'routes';
 import { useLocation } from 'react-router-dom';
 
 export const MovieItem = ({ item }) => {
@@ -10,7 +10,8 @@ export const MovieItem = ({ item }) => {
   return (
     <li className={s.item}>
       <Link
-        to={routes.moviedetails.replace(':movieId', item.id)}
+        // to={routes.moviedetails.replace(':movieId', item.id)}
+        to={`${item.id}`}
         state={{ from: location }}
       >
         <span className={s.title}>{item.title ?? item.original_title}</span>
